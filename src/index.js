bf760b29d412d7ffd0a5e79e561ed10fda142bf8
Lena <lenach241@gmail.com>
2018-09-11 14:04:31 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+	var count = 0;
+  for (n=1; n<=preferences.length; n++) {
+  	k1 = preferences[n-1];
+  	k2 = preferences[k1-1];
+  	k3 = preferences[k2-1];
+  	if ((k3 == n) && (k1!==k2) && (k2!==k3) && (k3!==k1)) {
+  		count++;
+  	} 
+  }
+  return count/3;
 };
